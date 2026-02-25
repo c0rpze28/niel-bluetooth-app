0698ad49f848c3ba320dc5b50f91c58560999a5c
c0rpze28 <gaffudcorpuz@gmail.com>
2026-02-25 21:30:58 +0800

    updated the version to 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,14 @@ plugins {
 
 android {
     namespace = "com.example.diddy_niel"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.diddy_niel"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
